af5b6c9c3f654b52694185d067fe37584a89e78a
xiaohe0601 <xiaohe0601@outlook.com>
2026-01-26 09:53:25 +0800

    chore: update deps

--- packages/eslint-config/tsdown.config.ts
+++ packages/eslint-config/tsdown.config.ts
@@ -3,15 +3,19 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: "./src/index.ts",
   platform: "node",
-  external: [
-    "eslint-plugin-perfectionist",
-    "find-up-simple"
-  ],
   format: [
     "esm",
     "cjs"
   ],
   dts: {
     build: true
-  }
+  },
+  inlineOnly: [
+    "es-toolkit"
+  ],
+  external: [
+    "eslint-flat-config-utils",
+    "eslint-plugin-perfectionist",
+    "find-up-simple"
+  ]
 });
--- packages/stylelint-config/tsdown.config.ts
+++ packages/stylelint-config/tsdown.config.ts
@@ -9,5 +9,8 @@ export default defineConfig({
   ],
   dts: {
     build: true
-  }
+  },
+  inlineOnly: [
+    "es-toolkit"
+  ]
 });
